75a62d1b0b28fa568a9076b4150cc505b0d7a214
yindaheng98 <yindaheng98@gmail.com>
2025-08-07 01:27:39 -0700

    run

--- run_shell_db.py
+++ run_shell_db.py
@@ -1,6 +1,13 @@
 import os
 
 for lmbda in [0.004]:  # Optionally, you can try: 0.003, 0.002, 0.001, 0.0005
-    for cuda, scene in enumerate(['playroom', 'drjohnson']):
-        one_cmd = f'CUDA_VISIBLE_DEVICES={0} python train.py -s data/blending/{scene} --eval --lod 0 --voxel_size 0.005 --update_init_factor 16 --iterations 30_000 -m outputs/blending/{scene}/{lmbda} --lmbda {lmbda}'
+    for cuda, scene in enumerate([
+        'coffee_martini/frame1',
+        'cook_spinach/frame1',
+        'cut_roasted_beef/frame1',
+        'flame_salmon_1/frame1',
+        'flame_steak/frame1',
+        'sear_steak/frame1',
+    ]):
+        one_cmd = f'python train.py -s data/{scene} --eval --lod 0 --voxel_size 0.005 --update_init_factor 16 --iterations 30_000 -m outputs/{scene}/{lmbda} --lmbda {lmbda}'
         os.system(one_cmd)
